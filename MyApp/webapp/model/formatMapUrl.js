1daf5af18196b7cf32fd20e1a5d8b2869bbfc587
Andrei Osipau <andrei.osipau@sap.com>
2016-06-10 14:03:38 +0000

    Week 2 Bonus (API key is removed)

--- MyApp/webapp/model/formatMapUrl.js
+++ MyApp/webapp/model/formatMapUrl.js
@@ -2,18 +2,14 @@ sap.ui.define([], function() {
 	"use strict";
 
 	return function(sHouseNumber, sStreet, sCity, sCountry) {
-		var APIkey = "AIzaSyA0S-vo46NgtBHvgk9ZNZaAOSS0_IlZ6Vc";
-
 		var url = "https://maps.googleapis.com/maps/api/staticmap?";
-		//API key
-		url += "key=" + APIkey;
-		//specify location
-		var location = sHouseNumber + "," + sStreet + "," + sCity + "," + sCountry;
-		url += "&markers=size:mid|color:red|" + encodeURIComponent(location);
 		//size
-		url += "&size=600x600";
+		url += "size=600x600";
 		//zoom
 		url += "&zoom=15";
+		//specify location
+		var location = sHouseNumber + "," + sStreet + "," + sCity + "," + sCountry;
+		url += "&markers=size:mid|color:red|" + $.sap.encodeURL(location);
 
 		return url;
 
